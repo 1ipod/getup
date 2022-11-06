f02178046bc0a9c14fb6e3708dc9406b45c7a9cc
that1ipodguy <77511422+that1ipodguy@users.noreply.github.com>
2022-11-06 08:30:52 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pacman -S thunar steam neovim fileroller
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S librewolf-bin j4-dmenu-desktop
+git clone https://github.com/that1ipodguy/.s-and-goodsoft.git
+cd .s-and-goodsoft
+cd dwm 
+make install
+cd ../
+cd st 
+make install
+cd ../
+cd ../
